da5a13ef339927827f0f2e683de608d7d1e6717c
Georgii Lebeda <lebeda.georgiy@gmail.com>
2025-02-19 23:20:33 +0000

    updated emplems

--- old-aged-footballers-frontend/src/components/GameDisplay.tsx
+++ old-aged-footballers-frontend/src/components/GameDisplay.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image';
 import { FaFutbol } from 'react-icons/fa';
 
 // Static team emblems (we can make these dynamic later)
-const TEAM_A_EMBLEM = 'https://resources.premierleague.com/premierleague/badges/rb/t20.svg';
-const TEAM_B_EMBLEM = 'https://resources.premierleague.com/premierleague/badges/rb/t91.svg';
+const TEAM_A_EMBLEM = 'https://resources.premierleague.com/premierleague/badges/rb/t21.svg'; // West Ham
+const TEAM_B_EMBLEM = 'https://resources.premierleague.com/premierleague/badges/rb/t6.svg';  // Tottenham
 
 interface GameDisplayProps {
   game: Game;
@@ -28,15 +28,16 @@ export function GameDisplay({ game, players, onGoalUpdate, isEditable = false }:
       <div className="flex flex-col sm:flex-row items-center justify-between mb-12">
         {/* Team A */}
         <div className="hidden sm:flex flex-col items-center w-1/3">
-          <div className="w-24 h-24 relative mb-4">
-            <Image
-              src={TEAM_A_EMBLEM}
-              alt="Bibs"
-              width={96}
-              height={96}
-              priority
-              className="object-contain"
-            />
+          <div className="w-24 h-24 relative mb-4 flex items-center justify-center">
+            <div className="relative w-20 h-20">
+              <Image
+                src={TEAM_A_EMBLEM}
+                alt="Bibs"
+                fill
+                priority
+                className="object-contain"
+              />
+            </div>
           </div>
           <h2 className="text-xl font-semibold">Bibs</h2>
         </div>
@@ -46,30 +47,32 @@ export function GameDisplay({ game, players, onGoalUpdate, isEditable = false }:
           {/* Mobile Team Icons + Score */}
           <div className="flex sm:hidden items-center space-x-4">
             <div className="flex items-center">
-              <div className="w-12 h-12 mr-3">
-                <Image
-                  src={TEAM_A_EMBLEM}
-                  alt="Bibs"
-                  width={48}
-                  height={48}
-                  priority
-                  className="object-contain"
-                />
+              <div className="w-12 h-12 relative mr-3 flex items-center justify-center">
+                <div className="relative w-10 h-10">
+                  <Image
+                    src={TEAM_A_EMBLEM}
+                    alt="Bibs"
+                    fill
+                    priority
+                    className="object-contain"
+                  />
+                </div>
               </div>
               <span className="text-4xl font-bold">{game.teamA.score}</span>
             </div>
             <span className="text-3xl font-light text-gray-400">-</span>
             <div className="flex items-center">
               <span className="text-4xl font-bold">{game.teamB.score}</span>
-              <div className="w-12 h-12 ml-3">
-                <Image
-                  src={TEAM_B_EMBLEM}
-                  alt="Shirts"
-                  width={48}
-                  height={48}
-                  priority
-                  className="object-contain"
-                />
+              <div className="w-12 h-12 relative ml-3 flex items-center justify-center">
+                <div className="relative w-10 h-10">
+                  <Image
+                    src={TEAM_B_EMBLEM}
+                    alt="Shirts"
+                    fill
+                    priority
+                    className="object-contain"
+                  />
+                </div>
               </div>
             </div>
           </div>
@@ -83,15 +86,16 @@ export function GameDisplay({ game, players, onGoalUpdate, isEditable = false }:
 
         {/* Team B */}
         <div className="hidden sm:flex flex-col items-center w-1/3">
-          <div className="w-24 h-24 relative mb-4">
-            <Image
-              src={TEAM_B_EMBLEM}
-              alt="Shirts"
-              width={96}
-              height={96}
-              priority
-              className="object-contain"
-            />
+          <div className="w-24 h-24 relative mb-4 flex items-center justify-center">
+            <div className="relative w-20 h-20">
+              <Image
+                src={TEAM_B_EMBLEM}
+                alt="Shirts"
+                fill
+                priority
+                className="object-contain"
+              />
+            </div>
           </div>
           <h2 className="text-xl font-semibold">Shirts</h2>
         </div>
