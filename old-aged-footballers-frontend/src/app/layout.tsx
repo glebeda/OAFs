5882a2e215022fe2813b003dc44fcd649af9d6b3
Georgii Lebeda <lebeda.georgiy@gmail.com>
2025-02-26 14:53:48 +0000

    adding analytics for vercel

--- old-aged-footballers-frontend/src/app/layout.tsx
+++ old-aged-footballers-frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { ClientLayout } from '@/components/ClientLayout';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -46,6 +47,7 @@ export default function RootLayout({
         <ClientLayout>
           {children}
         </ClientLayout>
+        <Analytics />
       </body>
     </html>
   );
